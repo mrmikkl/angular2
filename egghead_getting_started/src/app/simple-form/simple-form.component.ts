3a6c6c8ca6f28cb2cac401e8184b709cad34ec24
moimoi <xxx@gmail.com>
2016-12-13 16:56:05 +0100

    tut abgeschlossen

--- egghead_getting_started/src/app/simple-form/simple-form.component.ts
+++ egghead_getting_started/src/app/simple-form/simple-form.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  /**
+   * Default ist ViewEncapsulation.Emulated => Css von außen (globale Styles) gilt auch für Komponenten, Css von Komponenten nur für diese
+   * ViewEncapsulation.Native => Komponenten blockieren Css von außen (also die globalen Styles)
+   * ViewEncapsulation.None => von außen in Komponenten und von Komponenten nach außen
+   */
+  encapsulation: ViewEncapsulation.Native,
   selector: 'app-simple-form',
   templateUrl: './simple-form.component.html',
   styleUrls: ['./simple-form.component.css']
@@ -10,6 +16,8 @@ export class SimpleFormComponent implements OnInit {
   @Input() message;
 
   @Output() update = new EventEmitter();
+
+  isMousedown;
 
   constructor() { }
 
